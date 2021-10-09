80b95d4b2e06cf0ef7d97d06e1fdb5d6f49ac7b9
Christian Ilagan <cmilagan7@gmail.com>
2021-10-09 16:10:38 +1100

    login page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,31 @@
+console.log("hello");
+
 const config = require('./config.json');
 // A helper you may want to use when uploading new images to the server.
 import { fileToDataUrl } from './helpers.js';
 
 console.log('Let\'s go!');
+
+/**
+ * If the user is not logged in, display login/register pages
+ */
+function notLoggedIn() {
+    document.getElementById("login").style.display ="block";
+    console.log("notLoggedIn");
+}
+
+/**
+ * Check that a user has been logged in.
+ */
+
+function checkUserLogin() {
+    if (localStorage.key("token") === null) {
+        console.log("checked in here");
+        notLoggedIn();
+    } else {
+        // loggedIn();
+    }
+}
+
+console.log("hello");
+notLoggedIn();
